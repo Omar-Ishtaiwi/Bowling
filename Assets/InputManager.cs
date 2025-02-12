e79a1b48cc01d9fd1e7c73c539b9fcd66e06f66b
Omar Ishtaiwi <omar7672@student.ubc.ca>
2025-02-11 21:10:11 -0800

    Initial commit

---
+++ Assets/InputManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public UnityEvent OnSpacePressed = new UnityEvent();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+            OnSpacePressed?.Invoke();
+        }
+    }
+}
