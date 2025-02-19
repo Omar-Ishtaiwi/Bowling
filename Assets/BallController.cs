99eb0d9c32a9f4d9bbe512674381e466efd52c50
Omar Ishtaiwi <omar7672@student.ubc.ca>
2025-02-19 04:03:54 -0800

    Launch Indicator

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,30 +1,34 @@
-  using UnityEngine;
-  using UnityEngine.Events;
- 
- 
-  public class BallController : MonoBehaviour
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class BallController : MonoBehaviour
   {
   [SerializeField] private float force = 1f;
- 
-  // name booleans like a question
-  private bool isBallLaunched;
+  [SerializeField] private Transform ballAnchor;
+  [SerializeField] private Transform launchIndicator;
+
+
+    private bool isBallLaunched;
   private Rigidbody ballRB;
   private InputManager inputManager;
  
   void Start()
   {
   ballRB = GetComponent<Rigidbody>();
-    inputManager.OnSpacePressed.AddListener(LaunchBall);
+  inputManager.OnSpacePressed.AddListener(LaunchBall);
+  transform.parent = ballAnchor;
+  transform.localPosition = Vector3.zero;
+  ballRB.isKinematic = true;
   }
- 
- 
+  
   private void LaunchBall()
   {
-      // now your if check can be framed like a sentence
-  // "if ball is launched, then simply return and do nothing"
-  if (isBallLaunched) return;
-      // "now that the ball is not launched, set it to true and launch the ball"
-  isBallLaunched = true;
-      ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
-      }
+      if (isBallLaunched) return;
+      isBallLaunched = true;
+      transform.parent = null;
+      ballRB.isKinematic = false;
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+          launchIndicator.gameObject.SetActive(false);
+    }
   }
